79576f8b584cbf05571eec22138438f82111832f
LuisDiego24 <100032658+LuisDiego24@users.noreply.github.com>
2022-02-20 13:51:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ variables.pop(variables.index('servicio_alcantarillado'))
 variables.pop(variables.index('recoleccion_basuras'))
 variables.pop(variables.index('servicio_internet'))
 
-opcion_z = st.sidebar.selectbox(label="",
+opcion_z = st.sidebar.selectbox(label="auto seleccionado",
                                     options=variables)
 
 st.header("Grafica estrato social en su municipio: ")
